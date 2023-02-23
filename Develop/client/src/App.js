a7bc7b700797ab040a5e7e6249801244597df1f2
Tom Carlson <tomcarlson171@gmail.com>
2023-02-23 17:28:21 -0600

    MAde changes to App.js

--- Develop/client/src/App.js
+++ Develop/client/src/App.js
@@ -3,28 +3,38 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+
+const client = new ApolloClient({
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    });
+  },
+  uri: '/graphql'
+});
+
+
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
-          <Route 
-            path='*'
-            element={<h1 className='display-2'>Wrong page!</h1>}
-          />
-        </Routes>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <Navbar />
+          <div className="container">
+            <Route exact path="/" component={SearchBooks}/>
+            <Route exact path="/saved" component={SavedBooks}/>
+          </div>
+        </div>
+      </Router>
+    </ApolloProvider>
+              
   );
 }
 
