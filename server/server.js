de7470a73f8f9e9d06a8b9ed7372163b67737320
Tom Carlson <tomcarlson171@gmail.com>
2023-02-23 22:52:38 -0600

    add change to server.js

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
+server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
