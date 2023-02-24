a99b0df6ad05ea6fd7aa031a08bf3fc414dee9d5
Tom Carlson <tomcarlson171@gmail.com>
2023-02-23 22:58:01 -0600

    commit for heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+
 const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
